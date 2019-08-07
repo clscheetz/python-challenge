b876269b0bfa84a17e37019dbfd1d22ddcdda9f4
Chelsea Scheetz <clscheetz@gmail.com>
2019-08-06 21:25:43 -0400

    changes to homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,9 +14,6 @@ with open(csvpath, newline="") as csvfile:
     # Initialize veriables
     totalVotesCasted = 0
     candidatesWithVotes = {}
-    percentPerCandidate = 0.0
-    totalWinsPerCandidate = 0
-    winnerOfElection = ""
 
     # Loop through for total votes casted
     for row in csvreader:
@@ -27,18 +24,22 @@ with open(csvpath, newline="") as csvfile:
         if row[2] not in candidatesWithVotes.keys():
             candidatesWithVotes[row[2]] = 0
 
+        candidatesWithVotes[row[2]] = candidatesWithVotes[row[2]] + 1
 
+def printResults(resultsFile, result):
+    print(result)
+    resultsFile.write(result + "\n")
 
+with open("Results.txt", "w") as resultsFile:
+    printResults(resultsFile, "Election Results")
+    printResults(resultsFile, "-" * 25)
+    printResults(resultsFile, f"Total Votes: {totalVotesCasted:,}")
+    printResults(resultsFile, "-" * 25)
 
+    for candidate in candidatesWithVotes.keys():
+        winPercentage = candidatesWithVotes[candidate] / totalVotesCasted
+        printResults(resultsFile, f"{candidate}: {winPercentage:.3%} ({candidatesWithVotes[candidate]:,})")
 
-
-
-
-
-    print("Election Results")
-    print("-" * 25)
-    print(f"Total Votes: {totalVotesCasted:,}")
-    print("-" * 25)
-
-    for candidate in candidatesWithVotes:
-        print(f"{candidate}: ")
+    printResults(resultsFile, "-" * 25)
+    printResults(resultsFile, "Winner: " + max(candidatesWithVotes, key=candidatesWithVotes.get))
+    printResults(resultsFile, "-" * 25)
